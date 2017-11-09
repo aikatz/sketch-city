91c63c845b73e9e040144c3d85dea3c68ad2de82
Alex Katz <xanderkatz97@gmail.com>
2017-11-08 21:58:30 -0500

    updated the maze sim scripts

--- src/genmaze_ascii.py
+++ src/genmaze_ascii.py
@@ -3,8 +3,8 @@ import sys
  
 def make_maze(w = 16, h = 8):
     vis = [[0] * w + [1] for _ in range(h)] + [[1] * (w + 1)]
-    ver = [["# "] * w + ['#'] for _ in range(h)] + [[]]
-    hor = [["##"] * w + ['#'] for _ in range(h + 1)]
+    ver = [["10"] * w + ['1'] for _ in range(h)] + [[]]
+    hor = [["11"] * w + ['1'] for _ in range(h + 1)]
  
     def walk(x, y):
         vis[y][x] = 1
@@ -13,8 +13,8 @@ def make_maze(w = 16, h = 8):
         shuffle(d)
         for (xx, yy) in d:
             if vis[yy][xx]: continue
-            if xx == x: hor[max(y, yy)][x] = "# "
-            if yy == y: ver[y][max(x, xx)] = "  "
+            if xx == x: hor[max(y, yy)][x] = "10"
+            if yy == y: ver[y][max(x, xx)] = "00"
             walk(xx, yy)
  
     walk(randrange(w), randrange(h))
@@ -25,6 +25,6 @@ def make_maze(w = 16, h = 8):
     return s
  
 if __name__ == '__main__':
-    print("\x1b[34m")
+    #print("\x1b[34m")
     print(make_maze(int(sys.argv[1]), int(sys.argv[2])))
-    print("\x1b[0m")
+    #print("\x1b[0m")
--- src/test.py
+++ src/test.py
@@ -7,73 +7,29 @@ import locale
 locale.setlocale(locale.LC_ALL,"")
 
 maze_str = \
-u"""█████████████████████████████████████████████████████████████████████████████████████████████████████
-█   █   █                         █     █         █     █       █   █     █     █ █   █           █ █
-█ █ █ █ █████████████ ███████████ █ █ █ █ ███████ ███ █ ███ ███ █ █ █ █ ███ █ █ █ █ █ █ █ ███████ █ █
-█ █   █           █   █         █ █ █ █ █ █     █   █ █   █ █ █ █ █ █ █     █ █ █ █ █   █     █   █ █
-█ █████ █████████ █ █████ ███ ███ ███ █ █ █████ ███ █ ███ █ █ █ █ ███ ███████ █ █ █ █████████ █ ███ █
-█     █ █   █   █ █     █ █ █         █   █   █     █   █   █   █     █       █   █ █       █ █   █ █
-█ ███ ███ █ █ █ █ █████ █ █ ███████████████ █ █ █████ █ █████ ███ █████ ███████ ███ █ █████ █ ███ █ █
-█ █ █ █   █ █ █ █ █     █   █ █       █   █ █ █     █ █   █ █ █     █ █ █ █     █   █ █   █ █ █     █
-█ █ █ █ ███ █ █ █ █ ███████ █ █ █████ █ █ █ █ █████ █████ █ █ █████ █ █ █ █ █████ █████ █ █ █ █████ █
-█   █   █   █ █ █   █       █ █ █   █ █ █ █ █     █       █ █   █     █ █   █   █ █     █ █ █     █ █
-█████████ ███ █ █████ ███████ █ █ █ █ █ █ █ █████ █████████ ███ █████ █ █████ █ █ ███ ███ █ █████ ███
-█         █ █ █     █   █     █ █ █ █   █   █   █ █           █ █   █ █       █   █   █     █   █   █
-█ █ ███████ █ █████ ███ █ ███ █ █ █ █████████ ███ █████ █ █████ █ █ █ ███████ █████ █ █████ █ █ ███ █
-█ █ █     █ █     █ █   █   █ █   █       █     █     █ █     █   █ █   █   █     █ █ █   █   █ █   █
-█ ███ ███ █ █████ █ █ ███ █ █████████████ █ ███ █████ █ █████ █████ █████ █ █████ █ ███ █ ███ ███ █ █
-█ █   █ █ █       █   █   █           █   █   █     █ █     █             █     █   █   █   █ █   █ █
-█ █ ███ █ █████████ █████████████ █ ███ █████ ███ █ █ ███ █████████████████████ █ ███ █████ ███ ███ █
-█ █     █         █               █ █   █       █ █ █ █   █       █     █     █ █ █   █   █   █ █   █
-█ █████ █████████ █████████████ █████ ███████████ █ █ █████ █████ █ ███ █ █████ █ █ ███ █████ █ █████
-█   █   █     █   █   █     █   █     █           █ █   █   █   █   █   █ █     █ █ █ █     █ █     █
-█ █ █ ███████ █ ███ █ █ ███ █ ███ █████ █████ █████████ █ ███ █ █████ ███ █ ███████ █ █ █ █ █ ███ █ █
-█ █ █ █       █     █ █ █   █ █   █     █     █         █     █ █   █   █           █ █ █ █ █   █ █ █
-█ █ █ █ █████ ███████ ███ ███ █ █████ █████████ ███████ ███████ █ █ ███ █████████████ █ █ █████ ███ █
-█ █ █ █ █   █   █   █   █ █   █   █   █       █ █     █   █       █   █ █       █       █ █   █ █   █
-█ ███ █ █ █ ███ █ █████ █ █ █████ █ ███ █████ █ █ ███ █ ███ ███████████ █ █████ █ ███████ █ █ █ █ ███
-█     █ █ █   █   █     █   █       █   █   █ █   █   █ █   █       █   █ █ █   █ █       █ █   █   █
-█ █████ █ ███████ █ █████ ███ ███████ ███ ███ █████ ███ █ ███ █████ █ ███ █ █ █████ █████ █ ███████ █
-█       █       █ █ █     █   █     █ █ █         █ █   █   █ █ █   █   █   █   █   █     █   █   █ █
-█████████ █████ █ █ ███ ███ ███ ███ █ █ █ ███████ █ ███ ███ █ █ █ █████ ███ ███ █ ███ ███████ █ █ █ █
-█ █     █   █ █   █   █   █ █   █ █   █   █   █   █   █   █ █ █         █ █   █ █   █     █   █ █ █ █
-█ █ █ █████ █ ███████ █████ █ ███ █████ ███ █ █ █████ █████ █ █████████ █ ███ █ █ █ █████ █ ███ █ █ █
-█   █     █   █     █ █     █ █         █   █ █       █   █ █     █         █ █ █ █ █   █ █     █   █
-█ █████ ███ ███ ███ █ █ █████ ███████ ███ ███ ███ █████ █ █ █████ ███████████ █ █ █ █ █ ███████████ █
-█ █   █     █   █ █ █ █   █ █       █ █   █   █   █   █ █   █     █   █       █ █ █ █ █       █ █   █
-█ █ █ ███████ ███ █ █ ███ █ ███████ ███ ███ ███████ █ █ █████ █ ███ █ █ ███████ ███ █ ███ ███ █ █ ███
-█ █ █       █   █   █   █     █   █     █ █       █ █ █   █   █ █   █   █ █     █   █ █ █   █   █   █
-█ █████████ ███ ███ █ █ █████ █ █ ███████ ███████ █ █ ███ █ █████ ███████ █ █████ █ █ █ ███ ███ ███ █
-█ █   █   █   █   █   █   █   █ █         █     █   █ █   █       █   █   █ █     █ █ █   █   █   █ █
-█ █ █ █ █ ███ ███ █████████ ███████ █████ █████ █████ █ █████████████ █ ███ █ ███████ █ █ ███ █████ █
-█   █   █       █                   █               █                 █     █           █   █       █
-█████████████████████████████████████████████████████████████████████████████████████████████████████"""
+"""11111111111
+10000000001
+10111111101
+10001000101
+10101010111
+10101010101
+10101010101
+10101010001
+10101011111
+10100000001
+11111111111"""
 
 #maze_str = \
-u"""█████████████████████
-█       █     █     █
-█ ███ ███ █ ███ ███ █
-█   █     █     █ █ █
-███ ███████ █████ █ █
-█   █     █   █   █ █
-█ ███ ███ ███ ███ █ █
-█   █ █ █   █     █ █
-███ █ █ ███████████ █
-█   █ █       █     █
-█ ███ █ █████ █ ███ █
-█ █ █ █     █ █ █   █
-█ █ █ █ ███ █ █ ███ █
-█ █ █ █ █   █ █   █ █
-█ █ █ █ █ ███ ███ █ █
-█   █ █ █ █       █ █
-███ █ █ █ █████████ █
-█ █ █ █ █     █     █
-█ █ █ ███████ █ █████
-█   █         █     █
-█████████████████████"""
+"""11111
+10001
+11101
+10001
+10101
+10101
+11111"""
 
 # Turn maze_str into 2d list
-maze = [[c for c in row] for row in maze_str.split("\n")]
+maze = [[True if c == "1" else False for c in row] for row in maze_str.split("\n")]
 
 def maze_simulation(screen, start_x=1, start_y=1):
   # Set up curses stuff 
@@ -82,32 +38,104 @@ def maze_simulation(screen, start_x=1, start_y=1):
   height, width = screen.getmaxyx()
   curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
   curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
-  curses.init_pair(3, curses.COLOR_BLUE, curses.COLOR_BLACK)
+  curses.init_pair(3, curses.COLOR_RED, curses.COLOR_BLACK)
+  curses.init_pair(4, curses.COLOR_BLUE, curses.COLOR_BLACK)
+
+  # Simple wrapper for drawing a wall
+  def drawWall(wall_pos):
+    screen.addstr(wall_pos[0], wall_pos[1], u"\u2588".encode("utf-8"), curses.color_pair(1))
 
   # Draw maze initially
   for y in xrange(len(maze)):
     for x in xrange(len(maze[0])):
-      screen.addstr(y, x, maze[y][x].encode("utf-8"), curses.color_pair(1))
+      screen.addstr(y, x, "?", curses.color_pair(1))
   screen.refresh()
 
-  visited, stack = set(), [(start_y, start_x)]
+  # A set of all the visited positions
+  visited = set()
 
-  while stack:
-    pos = stack.pop()
+  # List of tuples corresponding to positions to move to
+  move_stack = [(start_y, start_x)]
+
+  # A stack that contains the movement history_stack of the robot
+  history_stack = []
+
+  # A stack that contains the position of intersections
+  intersect_stack = []
+
+  # The position of the robot, initalized to the upper left
+  pos = (start_y, start_x)
+
+  # A flag used to determine whether to backtrack to prev intersection
+  backtrack = False
+
+  while move_stack:
+    if backtrack:
+      # We need to backtrack, so select appropriate range of history 
+      # and then add it onto the move stack
+      history_stack.pop()
+      tmp = history_stack.pop()
+      back = intersect_stack.pop()
+      tmp_list = []
+      while(tmp != back):
+        tmp_list.append(tmp)
+        tmp = history_stack.pop()
+      history_stack.append(tmp)
+      move_stack += list(reversed(tmp_list))
+      backtrack = False
+
+    else:
+      pos = move_stack.pop()
+      history_stack.append(pos)
+
+    # Draw robot position
     screen.addstr(pos[0], pos[1], u"\u2588".encode("utf-8"), curses.color_pair(3))
     if pos not in visited:
       visited.add(pos)
-      if((pos[0]-1, pos[1]) not in visited and maze[pos[0]-1][pos[1]] == " "):
-        stack.append((pos[0]-1, pos[1]))
-      if((pos[0], pos[1]-1) not in visited and maze[pos[0]][pos[1]-1] == " "):
-        stack.append((pos[0], pos[1]-1))
-      if((pos[0]+1, pos[1]) not in visited and maze[pos[0]+1][pos[1]] == " "):
-        stack.append((pos[0]+1, pos[1]))
-      if((pos[0], pos[1]+1) not in visited and maze[pos[0]][pos[1]+1] == " "):
-        stack.append((pos[0], pos[1]+1))
+      stack_size = len(move_stack)
+
+      # Check "North"
+      if(not maze[pos[0]-1][pos[1]] and (pos[0]-1,pos[1]) not in visited):
+        move_stack.append((pos[0]-1, pos[1]))
+      elif((pos[0]-1,pos[1]) not in visited):
+        drawWall((pos[0]-1, pos[1]));
+
+      # Check "West"
+      if(not maze[pos[0]][pos[1]-1] and (pos[0],pos[1]-1) not in visited):
+        move_stack.append((pos[0], pos[1]-1))
+      elif((pos[0],pos[1]-1) not in visited):
+        drawWall((pos[0], pos[1]-1));
+
+      # Check "South"
+      if(not maze[pos[0]+1][pos[1]] and (pos[0]+1,pos[1]) not in visited):
+        move_stack.append((pos[0]+1, pos[1]))
+      elif((pos[0]+1,pos[1]) not in visited):
+        drawWall((pos[0]+1, pos[1]));
+
+      # Check "East"
+      if(not maze[pos[0]][pos[1]+1] and (pos[0],pos[1]+1) not in visited):
+        move_stack.append((pos[0], pos[1]+1))
+      elif((pos[0],pos[1]+1) not in visited):
+        drawWall((pos[0], pos[1]+1));
+
+      # figure out if we're at intersection or dead-end
+      if(len(move_stack) - stack_size == 0):
+        backtrack = True
+      if(len(move_stack) - stack_size > 1):
+        intersect_stack.append(pos)
+
+    # Update the screen, and wait for user to press a key
     screen.refresh()
-    time.sleep(.1)
+    c = screen.getch()
+
+    # Change position to path
     screen.addstr(pos[0], pos[1], u"\u2588".encode("utf-8"), curses.color_pair(2))
+
+  # Set color of robot to blue since we're done
+  screen.addstr(pos[0], pos[1], u"\u2588".encode("utf-8"), curses.color_pair(4))
+  screen.refresh()
+
+  # Maze is explored
   while True:
     continue
 
